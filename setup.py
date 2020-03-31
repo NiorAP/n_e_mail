a1955c5777dcd75258e144dcb1d3dbed804d4bb6
Nior.A.P <nior.a.p@hotmail.com>
2020-03-31 17:32:56 +0700

    Edit url link in setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='ap_e_mail',
-    version='2020.03.22',
+    version='2020.03.31',
     packages=['ap_e_mail'],
-    url='https://github.com/NiorAP/ap_line_notify',
+    url='https://github.com/NiorAP/ap_e_mail',
     license='MIT',
     author='Nior.A.P',
     author_email='nior.a.p@hotmail.com',
